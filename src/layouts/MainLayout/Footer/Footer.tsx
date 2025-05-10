4ad028f5a1e4f6cbb3bbf99b2f24ef19513683e9
nym-tmn <nymtmn8690@gmail.com>
2025-05-11 00:13:22 +0500

    Added styles for footer

--- src/layouts/MainLayout/Footer/Footer.tsx
+++ src/layouts/MainLayout/Footer/Footer.tsx
@@ -1,5 +1,39 @@
+import styled from "styled-components"
+import { Flex } from "@components"
+
+const StyledFooter = styled.div`
+background-color: black;
+color: white;
+font-size: 14px;
+`
+
+const FooterLink = styled.a`
+  text-decoration: underline;
+  text-transform: uppercase;
+  transition: opacity 0.3s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
 export function Footer() {
+
+	const baseUrl: string = import.meta.env.VITE_API_BASE_URL;
+
 	return (
-		<div>Footer</div>
+		<StyledFooter>
+			<Flex $height="50px" $justify="space-between" $align="center" $margin="0 370px">
+				<FooterLink href={`${baseUrl}/character`} target="_blank" rel="noopener noreferrer">
+					characters 826
+				</FooterLink>
+				<FooterLink href={`${baseUrl}/location`} target="_blank" rel="noopener noreferrer">
+					locations 126
+				</FooterLink>
+				<FooterLink href={`${baseUrl}/episode`} target="_blank" rel="noopener noreferrer">
+					episodes 51
+				</FooterLink>
+			</Flex>
+		</StyledFooter>
 	)
 }
