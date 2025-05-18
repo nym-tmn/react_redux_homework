1a0d26c2feefdb4736068546889333e38915016c
nym-tmn <nymtmn8690@gmail.com>
2025-05-18 19:04:03 +0500

    Using CSS-modules in Title component

--- src/layouts/MainLayout/Header/Title/Title.tsx
+++ src/layouts/MainLayout/Header/Title/Title.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router";
-import { styled } from "styled-components"
-
-const StyledTitle = styled.h1`
-font-weight: 600;
-font-size: 34px;
-`;
+import classes from '@layouts/MainLayout/Header/Title/Title.module.css';
 
 export const Title = React.memo(() => {
 
 	return (
-		<StyledTitle>
+		<h1 className={classes.title}>
 			<NavLink to="/">Rick and Morty API</NavLink>
-		</StyledTitle>
+		</h1>
 	)
 })
