a7d7ffcd424895cd17edba20c3b79abf47355573
nym-tmn <nymtmn8690@gmail.com>
2025-05-31 23:05:57 +0500

    Refactoring actions for characters

--- src/store/actions/characters.ts
+++ src/store/actions/characters.ts
@@ -1,50 +1,46 @@
 import { getCharacters, getFiltredCharacters } from "@api";
 import type { AppThunk } from "@store";
-import { CharactersActionTypes } from "@types";
+import { CharactersActionTypes, type CharactersType, type ResponseType } from "@types";
 import axios from "axios";
 
 // Async
 
 export const fetchCharacters = (currentPage: number): AppThunk => async (dispatch) => {
 	try {
-		dispatch({ type: CharactersActionTypes.FETCH_CHARACTERS })
+		dispatch(setIsLoading())
 		const response = await getCharacters(currentPage);
-		dispatch({ type: CharactersActionTypes.FETCH_CHARACTERS_SUCCESS, payload: response })
+		dispatch(setCharacters(response))
 	} catch (error) {
 		if (axios.isAxiosError(error)) {
 			if (error.response?.status === 404) {
-				dispatch({
-					type: CharactersActionTypes.FETCH_CHARACTERS_ERROR, payload: "Failed to load characters."
-				});
+				dispatch(setError("Failed to load characters."));
 			}
 		} else if (error instanceof Error) {
 			console.error(error.message);
-			dispatch({ type: CharactersActionTypes.FETCH_CHARACTERS_ERROR, payload: "An unexpected error occurred" });
+			dispatch(setError("An unexpected error occurred"));
 		} else {
 			console.error('Unknown error:', error);
-			dispatch({ type: CharactersActionTypes.FETCH_CHARACTERS_ERROR, payload: "Something went wrong" });
+			dispatch(setError("Something went wrong"));
 		}
 	}
 }
 
 export const fetchFiltredCharacters = (currentPage: number, debouncedSearchValue: string): AppThunk => async (dispatch) => {
 	try {
-		dispatch({ type: CharactersActionTypes.FETCH_CHARACTERS })
+		dispatch(setIsLoading())
 		const response = await getFiltredCharacters(currentPage, debouncedSearchValue);
-		dispatch({ type: CharactersActionTypes.FETCH_CHARACTERS_SUCCESS, payload: response })
+		dispatch(setCharacters(response))
 	} catch (error) {
 		if (axios.isAxiosError(error)) {
 			if (error.response?.status === 404) {
-				dispatch({
-					type: CharactersActionTypes.FETCH_CHARACTERS_ERROR, payload: `No characters found for "${debouncedSearchValue}"`
-				});
+				dispatch(setError(`No characters found for "${debouncedSearchValue}"`));
 			}
 		} else if (error instanceof Error) {
 			console.error(error.message);
-			dispatch({ type: CharactersActionTypes.FETCH_CHARACTERS_ERROR, payload: "An unexpected error occurred" });
+			dispatch(setError("An unexpected error occurred"));
 		} else {
 			console.error('Unknown error:', error);
-			dispatch({ type: CharactersActionTypes.FETCH_CHARACTERS_ERROR, payload: "Something went wrong" });
+			dispatch(setError("Something went wrong"));
 		}
 	}
 }
@@ -64,4 +60,18 @@ export const setCurrentPage = (currentPage: number) => ({
 export const setSearchInputValue = (searchInputValue: string) => ({
 	type: CharactersActionTypes.CHARACTERS_SET_SEARCH_INPUT_VALUE,
 	payload: searchInputValue,
+})
+
+const setIsLoading = () => ({
+	type: CharactersActionTypes.FETCH_CHARACTERS,
+})
+
+const setCharacters = (response: ResponseType<CharactersType>) => ({
+	type: CharactersActionTypes.FETCH_CHARACTERS_SUCCESS,
+	payload: response,
+})
+
+const setError = (errorMessage: string) => ({
+	type: CharactersActionTypes.FETCH_CHARACTERS_ERROR,
+	payload: errorMessage,
 })
